fe7f1feede3c4255d1e660beda58d31d0868b300
Pushkar Kopparla <pushkarkopparla@gmail.com>
2021-09-09 16:13:12 +0200

    Added nodatafill

--- src/process.py
+++ src/process.py
@@ -1,9 +1,48 @@
 from glob import glob
 import os
+import subprocess
 
-stackcommand = "gdal_merge.py -o rgb.tif -separate -co PHOTOMETRIC=RGB -co COMPRESS=DEFLATE {red} {green} {blue}"
-tcicommand = "gdal_translate -scale 0 18000 -exponent 0.5 -ot Byte -of JPEG -outsize 10% 10% rgb.tif tci.jpg"
-cleanupcommand = "rm rgb.tif"
+stackcommand = "gdal_merge.py -o rgb.tif -ot Byte -separate -co PHOTOMETRIC=RGB -co COMPRESS=DEFLATE {red} {green} {blue}"
+tcicommand = "gdal_translate -scale 20 80 0 255 -exponent 0.5 -ot Byte -of JPEG -outsize 10% 10% rgb.tif tci.jpg"
+# tcicommand = "gdal_translate -scale 20 0 255 -of JPEG -outsize 10% 10% rgb.tif tci.jpg"
+cleanupcommand = "rm *.tif*"
+
+
+def getminmax(tiffile):
+    """function to min and max values of the raster using gdalinfo"""
+    f = (
+        subprocess.check_output("gdalinfo -stats {0}".format(tiffile), shell=True)
+        .decode("utf-8")
+        .split("\n")
+    )
+    for strings in f:
+        if strings.find("STATISTICS_MINIMUM") >= 0:
+            min = strings.split("=")[-1]
+        if strings.find("STATISTICS_MAXIMUM") >= 0:
+            max = strings.split("=")[-1]
+    return [min, max]  # note that the list elements are strings
+
+
+def scaleandfill(tiffiles):
+    """function to fill no data and scale values from 0 to 1"""
+    outfilelist = []
+    for tiffile in tiffiles:
+        nodatafilename = "nodata" + tiffile[-6:]
+        scaledfilename = "scaled" + tiffile[-6:]
+
+        nodatacommand = "gdal_fillnodata.py {infile} {outfile}"
+        os.system(nodatacommand.format(infile=tiffile, outfile=nodatafilename))
+        min, max = getminmax(nodatafilename)
+        scalingcommand = (
+            "gdal_translate -ot Byte -scale {min} {max} 0 255 {infile} {outfile}"
+        )
+        os.system(
+            scalingcommand.format(
+                min=min, max=max, infile=nodatafilename, outfile=scaledfilename
+            )
+        )
+        outfilelist.append(scaledfilename)
+    return outfilelist
 
 
 def getpreview(dir="download"):
@@ -17,7 +56,8 @@ def getpreview(dir="download"):
         blue,
         green,
         red,
-    ) = bandfiles  # unpack the bands in to the three colors red, blue and green oninllnaegag
+    ) = scaleandfill(bandfiles)
+    # unpack the bands in to the three colors red, blue and green oninllnaegag
     os.system(stackcommand.format(red=red, green=green, blue=blue))
     os.system(tcicommand)
     os.system(cleanupcommand)
--- temp.py
+++
@@ -1,9 +0,0 @@
-def func(backend_kwargs=None, **kwargs):
-    if backend_kwargs is not None:
-        kwargs.update(backend_kwargs)
-    print(kwargs)
-    return None
-
-
-if __name__ == "__main__":
-    func()
